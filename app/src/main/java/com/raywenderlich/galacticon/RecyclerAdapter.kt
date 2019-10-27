4293865673df5b7b526e4a72bda399a691e90573
Admin <heinerbauza@gmail.com>
2019-10-26 20:37:51 -0700

    Done

--- app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
+++ app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
@@ -1,16 +1,54 @@
 package com.raywenderlich.galacticon
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
 class RecyclerAdapter(private val photos: ArrayList<Photo>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val inflateView = parent.inflate(R.layout.recyclerview_item_row)
+        return ViewHolder(inflateView)
     }
 
     override fun getItemCount() = photos.size
 
-    override fun onBindViewHolder(p0: RecyclerAdapter.ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        val itemPhoto = photos[position]
+        holder.bindPhoto(itemPhoto)
     }
+
+
+  class ViewHolder(v : View) : RecyclerView.ViewHolder(v) , View.OnClickListener {
+      private var view : View = v
+      private var photo : Photo? = null
+
+      init {
+          v.setOnClickListener(this)
+      }
+
+      override fun onClick(v: View?) {
+          val context = itemView.context
+          val showPhotoIntent = Intent(context, PhotoActivity::class.java)
+          showPhotoIntent.putExtra(PHOTO_KEY, photo)
+          context.startActivity(showPhotoIntent)
+      }
+
+      companion object{
+          private val PHOTO_KEY = "PHOTO"
+      }
+
+      fun bindPhoto(photo: Photo){
+          this.photo = photo
+          Picasso.with(view.context).load(photo.url).into(view.itemImage)
+          view.itemDate.text = photo.humanDate
+          view.itemDescription.text = photo.explanation
+      }
+  }
+
+
 }
